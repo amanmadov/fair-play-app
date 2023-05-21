a60296d13806e5d464a595a523f6c6807e2445ea
Nury Amanmadov <amanmadov@gmail.com>
2023-05-21 01:51:14 -0700

    create: calculate functionality

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
-let playerCounter = 1;
+class Player {
+    constructor(name, skillLevel) {
+        this.name = name;
+        this.skillLevel = skillLevel;
+    }
+}
+
+const playerListDiv = document.getElementById("playerList");
+const btnAddPlayer = document.getElementById("btnAddPlayer");
+const btnGetTeams = document.getElementById("btnGetTeams");
+const team1Table = document.getElementById("table1");
+const team2Table = document.getElementById("table2");
+const outputTables = document.getElementById("outputTables");
+
+let classNames = Array.from(btnGetTeams.classList);
+
+// initial values
+let playerCount = 1;
+const team1 = [];
+const team2 = [];
+let totalSkillTeam1 = 0;
+let totalSkillTeam2 = 0;
+
 
 function addPlayerFields() {
-    const playerList = document.getElementById("playerList");
 
-    //#region InputGroup
+    //#region Build InputGroup String
     const inputGroup = `<div class="row justify-content-md-center">
-                            <div class="col-6">
+                            <div class="col col-md-6 col-sm-12">
                                 <div class="input-group mb-6 p-1">
-                                    <span class="input-group-text">${playerCounter}</span>
+                                    <span class="input-group-text">${playerCount}</span>
                                     <input type="text" placeholder="Player Name" class="form-control"
                                         aria-label="Text input with dropdown button">
                                         <select class="form-select" aria-label="Default select example">
-                                            <option selected>Player Skill Level</option>
+                                            <option value="default" selected>Player Skill Level</option>
                                             <option value="1">1</option>
                                             <option value="2">2</option>
                                             <option value="3">3</option>
@@ -28,28 +49,133 @@ function addPlayerFields() {
                         </div>`;
     //#endregion
 
+    //#region Append element to DOM 
+
     let row = document.createElement('div');
     row.innerHTML = inputGroup;
     row = row.firstChild;
-    playerList.appendChild(row);
-    // updateDropdownValue();
-    playerCounter++;
+    playerListDiv.appendChild(row);
+
+    //#endregion
+
+    playerCount++;
+    if (playerCount >= 3 && classNames.includes('d-none')) btnGetTeams.classList.remove("d-none");
+
 }
 
-function updateDropdownValue() {
-    const dropdownMenu = document.querySelector('.dropdown-menu');
-    const dropdownItems = dropdownMenu.querySelectorAll('.dropdown-item');
-    const dropdownButton = document.getElementById('btnSelect');
+function validateInputs() {
+    const inputs = document.querySelectorAll('input[type="text"]');
+    const selects = document.querySelectorAll('select');
 
-    dropdownItems.forEach(item => {
-        item.addEventListener('click', function () {
-            const selectedValue = this.textContent;
-            dropdownButton.textContent = selectedValue;
-        });
+    // Check if all input fields are not empty
+    for (const input of inputs) {
+        if (input.value.trim() === '') return false;
+    }
+
+    // Check if all select elements have a selected value other than the default value
+    for (const select of selects) {
+        if (select.value === 'default') return false;
+    }
+
+    return true;
+}
+
+function getPlayers() {
+    const playerElements = document.querySelectorAll('#playerList .input-group');
+    const players = [];
+
+    playerElements.forEach(playerElement => {
+        const playerName = playerElement.querySelector('input[type="text"]').value;
+        const level = parseInt(playerElement.querySelector('select').value);
+        const player = new Player(playerName, level);
+        players.push(player);
     });
+
+    return players;
+}
+
+function distributeTeams(players) {
+    players.sort((a, b) => b.skillLevel - a.skillLevel);
+    for (let i = 0; i < players.length; i++) {
+        const currentPlayer = players[i];
+        if (totalSkillTeam1 <= totalSkillTeam2) {
+            team1.push(currentPlayer);
+            totalSkillTeam1 += currentPlayer.skillLevel;
+        } else {
+            team2.push(currentPlayer);
+            totalSkillTeam2 += currentPlayer.skillLevel;
+        }
+    }
+
+    // return {
+    //     team1: team1,
+    //     team2: team2,
+    //     totalSkillTeam1: totalSkillTeam1,
+    //     totalSkillTeam2: totalSkillTeam2
+    // };
+}
+
+function printTeams() {
+    team1Table.innerHTML = generateTable(team1, totalSkillTeam1);
+    team2Table.innerHTML = generateTable(team2, totalSkillTeam2);
+}
+
+function generateTable(team, skillTotal) {
+    let rows = '';
+    for (const [index, player] of team.entries()) {
+        rows += `<tr>
+                    <th scope="row">${index + 1}</th>
+                    <td>${player.name}</td>
+                    <td>${player.skillLevel}</td>
+                </tr>`;
+    }
+
+    let table = `<table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Player</th>
+                            <th scope="col">Skill Level</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        ${rows}
+                    </tbody>
+                    <tfoot>
+                    <tr>
+                        <td colspan="2"></td>
+                        <td class="text-danger fw-bold">Total Skill: ${skillTotal}</td>
+                    </tr>
+                    
+                  </tfoot>
+                </table>`;
+    return table;
+}
+
+function resetAll() {
+    team1.length = 0;
+    team2.length = 0;
+    totalSkillTeam1 = 0;
+    totalSkillTeam2 = 0;
+    team1Table.innerHTML = '';
+    team2Table.innerHTML = '';
+}
+
+function process() {
+    resetAll();
+    let areInputsValid = validateInputs();
+    if (!areInputsValid) {
+        var myModal = new bootstrap.Modal(document.getElementById('myModal'));
+        myModal.show();
+        return;
+    }
+
+    let playerList = getPlayers();
+    distributeTeams(playerList);
+    printTeams();
+    outputTables.scrollIntoView();
 }
 
 
-
-
-document.getElementById("btnAddPlayer").addEventListener("click", addPlayerFields);
+btnAddPlayer.addEventListener("click", addPlayerFields);
+btnGetTeams.addEventListener("click", process);
